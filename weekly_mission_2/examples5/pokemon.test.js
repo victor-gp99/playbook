13becdf7851fcb9be23cad06766e0dd95cb614dd
vickvaporu <victoreduardogpat99@outlook.com>
2022-04-21 21:28:57 -0500

    corrigiendo pruebas unitarias w10

--- weekly_mission_2/examples5/pokemon.test.js
+++ weekly_mission_2/examples5/pokemon.test.js
@@ -1,6 +1,9 @@
 import Pokemon from './pokemon'
 
 test('1) Create a new object pokemon', () => {
-  const myPokemon = new Pokemon('Pikachu')
-  expect(myPokemon.name).toBe('Pikachussssssss'); // Corrige esta prueba
+   const myPokemon = new Pokemon('Pikachu')
+   //expect(myPokemon.name).toBe('Pikachussssssss'); // Corrige esta prueba esta truena porque lo que espero es pikachu
+   expect(myPokemon.name).toBe('Pikachu');
+  //expect(4+2).toBe(10);
+
 });
